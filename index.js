44476c282897dd1709d6f9683551c74650ba2691
Mark <mark.njau@student.moringaschool.com>
2024-07-14 20:48:21 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your code here
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users',{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({ name: name, email: email })
+    })
+    .then(response => response.json())
+    .then(data => {
+        const idElement = document.createElement('p');
+        idElement.textContent = `ID: ${data.id}`;
+        document.body.appendChild(idElement);
+    })
+    .catch(error => {
+        const errorMessage = document.createElement('p');
+        errorMessage.textContent = `Error: ${error.message}`;
+        document.body.appendChild(errorMessage);
+    });
+}
